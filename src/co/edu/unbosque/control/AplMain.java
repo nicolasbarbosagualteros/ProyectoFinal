4fc67f18a283d65a772aabe1f54f3fc7f26dac4a
giulianaalejo <galejo@unbosque.edu.co>
2024-04-17 21:14:55 -0500

    prueba

--- src/co/edu/unbosque/control/AplMain.java
+++ src/co/edu/unbosque/control/AplMain.java
@@ -3,5 +3,7 @@ package co.edu.unbosque.control;
 public class AplMain {
     public static void main(String[] args) {
         System.out.println("Buena perro");
+        System.out.println("Buena perro negro");
+
     }
 }
